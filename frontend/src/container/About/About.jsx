f49f31d110c6749c7f627a7327a10f7a85e28632
Aamir Muhammad Amin <aamir1401a@gamil.com>
2022-02-22 13:59:40 +0500

    completed about section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 import "./About.scss";
+import "./About-MediaQuery.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 const About = () => {
-  return <div>About</div>;
+  const abouts = [
+    {
+      title: "Frontend",
+      description:
+        "I am a Frontend Developer having more than 2.5 years of industrial experience. ",
+      imgUrl: images.about02,
+    },
+    {
+      title: "UI/UX",
+      description:
+        "I always ensure that the user Interface should be eye catching and the user experience should be smooth",
+      imgUrl: images.about03,
+    },
+    {
+      title: "Web Animations",
+      description:
+        "I am always excited to make a creative/attractive websites with animations",
+      imgUrl: images.about04,
+    },
+  ];
+
+  return (
+    <section className="app__section-about">
+      <h2 className="head-text">
+        I know That <span>Good Apps </span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default About;
