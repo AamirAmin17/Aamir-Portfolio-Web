e9539ee1a0be48393e8ebbc222e01cd84bfa51a6
Aamir Muhammad Amin <aamir1401a@gamil.com>
2022-02-22 17:31:12 +0500

    working on sanity client connection

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./About.scss";
 import "./About-MediaQuery.scss";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+import { client, urlFor } from "../../client";
 const About = () => {
-  const abouts = [
-    {
-      title: "Frontend",
-      description:
-        "I am a Frontend Developer having more than 2.5 years of industrial experience. ",
-      imgUrl: images.about02,
-    },
-    {
-      title: "UI/UX",
-      description:
-        "I always ensure that the user Interface should be eye catching and the user experience should be smooth",
-      imgUrl: images.about03,
-    },
-    {
-      title: "Web Animations",
-      description:
-        "I am always excited to make a creative/attractive websites with animations",
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  // const abouts = [
+  //   {
+  //     title: "Frontend Development",
+  //     description:
+  //       "I am a Frontend Developer having more than 2.5 years of industrial experience. ",
+  //     imgUrl: images.about02,
+  //   },
+  //   {
+  //     title: "UI/UX Development",
+  //     description:
+  //       "I always ensure that the user Interface should be eye catching and the user experience should be smooth",
+  //     imgUrl: images.about03,
+  //   },
+  //   {
+  //     title: "Web Animations",
+  //     description:
+  //       "I am always excited to make a creative/attractive websites with animations",
+  //     imgUrl: images.about04,
+  //   },
+  // ];
 
   return (
     <section className="app__section-about">
